0259838b6fcabb7780b7ba1e753151cf5185e2dd
ankita <awqhf@mail.umkc.edu>
2017-06-16 12:05:17 -0500

    add files

---
+++ ICE/ICE1/Circle.py
@@ -0,0 +1,10 @@
+# Program for reversing a number
+import math
+
+r = int(input("Please Enter a radius : ")) #Enter the radius of a circle
+
+Area = math.pi*r*r #calculating the area of a circle
+Perimeter = 2*math.pi*r #calculating the perimeter
+
+print(" Area of a circle is:",Area)
+print("Perimeter of a circle is:",Perimeter)
